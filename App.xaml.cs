7c35975dbb97089ad53e9d40034e74e60c1f1ed3
MartinLopezRamos <sluzhor.tablet@gmail.com>
2026-03-19 14:27:27 +0100

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ULTRA_MEGA_MOC_EFEKTIVNÍ_MAPA
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
